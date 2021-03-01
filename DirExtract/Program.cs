f3c0450b43623e886ec7548f9f268084b3f39d6b
studentam <studentam@DNET11>
2021-03-01 10:57:14 -0500

    Parellel threading and tasking

--- DirExtract/Program.cs
+++ DirExtract/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DirExtract
 {
     internal class Program
     {
+        private static System.Collections.Queue dirs = new System.Collections.Queue(), files = new System.Collections.Queue();
+        private static string outputPath;
         private static void Main(string[] args)
         {
         one:
@@ -18,31 +21,56 @@ namespace DirExtract
                 Console.WriteLine("Not found!"); goto one;
             }
 
-            string[] dirs = null, files = null;
+            Thread thread = new Thread(GetFiles);
+            Console.WriteLine();
+            thread.Start();
 
-            foreach(string s in Directory.EnumerateDirectories(Path))
+            foreach (string s in Directory.GetDirectories(Path, "", SearchOption.AllDirectories))
+                dirs.Enqueue(s);
+
+            Thread.Sleep(750);
+            thread.Join();
+
+            System.Collections.Generic.List<string> TheList = new System.Collections.Generic.List<string>(32);
+            while (files.Count > 0)
             {
-                //get the directories and loop though each one to get more directories
-                //then loop through each directorie for files, queue directorie 
+                string temp = files.Dequeue().ToString();
+                Console.WriteLine(temp);
+                TheList.Add(temp);
             }
                 
-                //Parallel.For(0, dirs.Length, (i, state) => Console.WriteLine(i));
-                //Parallel.For(0, files.Length, (j, state) => Console.WriteLine(j));
-            
+
+            Console.WriteLine("\nFinished!");
+            outputPath = Console.ReadLine();
+            foreach (string s in TheList)
+            {
+                Task task = Task.Factory.StartNew(DeployFile, s);
+            }
         }
 
-        static void getDirs()
+        private static void GetFiles()
         {
-
+            Thread.Sleep(500);
+            while (dirs.Count > 0)
+            {
+                string temp = dirs.Dequeue().ToString();
+                Task task = Task.Factory.StartNew(QueueFiles, temp);
+                Console.Write('.');
+            }
         }
 
-        //foreach(string s in Directory.GetDirectories(Path))
-        //{
-        //}
+        private static Action<object> QueueFiles = (object s) =>
+        {
+            foreach (string b in Directory.GetFiles(s as string))
+            {
+                if (b.EndsWith(".docx"))
+                    files.Enqueue(b);
+            }
+            
+        };
+        private static Action<object> DeployFile = (object s) =>
+        {
+            File.
+        };
     }
-
-    //gets a string of the found file
-    //private static async Task<string> GetDocName()
-    //{
-    //}
 }
