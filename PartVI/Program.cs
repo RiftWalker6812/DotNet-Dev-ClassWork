1d79c2dfa1b0510a374ad2fe929c62118dd82b26
studentam <studentam@DNET11>
2021-05-04 10:54:21 -0400

    ASYNC

--- PartVI/Program.cs
+++ PartVI/Program.cs
@@ -3,37 +3,36 @@ using System.Threading;
 
 namespace PartVI
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
             O1(); //Operation 1
+            O2(); //Operation 2
         }
 
         delegate int BinaryOp(int x, int y);
-        unsafe static void O1()
+        static void O1()
         {
-            Console.WriteLine("***** Synch Delegate Review *****");
+            Console.WriteLine("***** Async Delegate Invocation *****");
             // Print out the ID of the executing thread.
             Console.WriteLine("Main() invoked on thread {0}.",
             Thread.CurrentThread.ManagedThreadId);
-            // Invoke Add() in a synchronous manner.
-            //BinaryOp add = Add;
-            delegate*<int, int, int> add = &Add; //Function Pointer
-            // Could also write b.Invoke(10, 10);
-            int answer = add(10, 10);
-            add = null;
-            // These lines will not execute until
-            // the Add() method has completed.
-            Console.WriteLine("Doing more work in Main()!");
+            // Invoke Add() on a secondary thread.
+            BinaryOp b = new BinaryOp(Add);
+            IAsyncResult iftAR = b.BeginInvoke(10, 10, null, null);
+            // Do other work on primary thread...
+            while (!iftAR.AsyncWaitHandle.WaitOne(1000, true))
+                Console.WriteLine("Doing more work in Main()!");
+            // Obtain the result of the Add()
+            // method when ready.
+            int answer = b.EndInvoke(iftAR);
             Console.WriteLine("10 + 10 is {0}.", answer);
             Console.ReadLine();
 
             return;
-
-            static int Add(int x, int y) => x + y;
 
             #region UNUSED
             //static void ExtractExecutingThread()
@@ -43,6 +42,21 @@ namespace PartVI
             //static void ExtractCurrentThreadContext()
             //{ Context ctx = Thread.CurrentContext; }
             #endregion
+        }
+        static void O2()
+        {
+            Console.WriteLine("***** AsyncCallbackDelegate Example *****");
+            Console.WriteLine("Main() invoked on thread {0}.",
+            Thread.CurrentThread.ManagedThreadId);
+            BinaryOp b = new BinaryOp(Add);
+            IAsyncResult iftAR = b.BeginInvoke(10, 10, AddComplete, null);
+            // Assume other work is performed here...
+            while (!isDone)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Working....");
+            }
+            Console.ReadLine(); //Page 756
         }
     }
 }
---
+++ PartVI/SideQuest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace PartVI
+{
+    partial class Program
+    {
+        static bool isDone = false;
+        static int Add(int x, int y)
+        {
+            // Print out the ID of the executing thread.
+            Console.WriteLine("Add() invoked on thread {0}.",
+            Thread.CurrentThread.ManagedThreadId);
+            // Pause to simulate a lengthy operation.
+            Thread.Sleep(5000);
+            return x + y;
+        }
+        static void AddComplete(IAsyncResult itfAR)
+        {
+            Console.WriteLine("AddComplete() invoked on thread {0}.",
+            Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Your addition is complete");
+            isDone = true;
+        }
+    }
+}
