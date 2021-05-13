3975888b6ffefb1f1b39c962d8779ad98fcacaf3
studentam <studentam@DNET11>
2021-05-13 10:49:16 -0400

    Parallel Tasks

--- MultiThreadingTasks/Form1.Designer.cs
+++ MultiThreadingTasks/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace MultiThreadingTasks
         private void InitializeComponent()
         {
             this.btnProcessImages = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnProcessImages
@@ -42,11 +43,22 @@ namespace MultiThreadingTasks
             this.btnProcessImages.UseVisualStyleBackColor = true;
             this.btnProcessImages.Click += new System.EventHandler(this.btnProcessImages_Click);
             // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(142, 170);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(78, 49);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(371, 303);
+            this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnProcessImages);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +69,7 @@ namespace MultiThreadingTasks
         #endregion
 
         private System.Windows.Forms.Button btnProcessImages;
+        private System.Windows.Forms.Button btnCancel;
     }
 }
 
--- MultiThreadingTasks/Form1.cs
+++ MultiThreadingTasks/Form1.cs
@@ -21,30 +21,42 @@ namespace MultiThreadingTasks
 
         private void ProcessFiles()
         {
-            string[] files = Directory.GetFiles(@"C:\Users\studentam\Documents\Joshua Hernandez Work\2020-2021\STC Online\Book4\aPressCsharp60nAsp46\9781484213339\9781484213339_Ch19_CodeSamples\Chapter_19\TestPictures", "*.jpg", SearchOption.AllDirectories);
-            string newDir = @"C:\Users\studentam\Documents\Joshua Hernandez Work\2020-2021\STC Online\Book4\MultiThreadingTasks\bin\ModifiedPictures";
-            //Directory.CreateDirectory(newDir);
-
-            Parallel.ForEach(files, currentFile =>
+            ParallelOptions parOpts = new()
             {
-                using Bitmap bitmap = new(currentFile);
-                bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
-                bitmap.Save(Path.Combine(newDir, Path.GetFileName(currentFile)));
-            });
+                CancellationToken = cancelToken.Token,
+                MaxDegreeOfParallelism = Environment.ProcessorCount
+            };
 
-            //foreach (var currentFile in files)
-            //{
-            //    var filename = Path.GetFileName(currentFile);
-
-            //    using Bitmap bitmap = new(currentFile);
-            //    bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
-            //    bitmap.Save(Path.Combine(newDir, filename));
-                
-            //    Text = string.Format("Processing {0} on thread {1}", filename, Thread.CurrentThread.ManagedThreadId);
-            //    filename = null;
-            //}
+            string[] files = Directory.GetFiles(@"C:\Users\studentam\Documents\Joshua Hernandez Work\2020-2021\STC Online\Book4\aPressCsharp60nAsp46\9781484213339\9781484213339_Ch19_CodeSamples\Chapter_19\TestPictures", "*.jpg", SearchOption.AllDirectories);
+            var newDir = @"C:\Users\studentam\Documents\Joshua Hernandez Work\2020-2021\STC Online\Book4\MultiThreadingTasks\bin\ModifiedPictures";
+            try
+            {
+                Parallel.ForEach(files, parOpts, currentFile =>
+                {
+                    parOpts.CancellationToken.ThrowIfCancellationRequested();
+                    var filename = Path.GetFileName(currentFile);
+                    using Bitmap bitmap = new(currentFile);
+                    bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                    bitmap.Save(Path.Combine(newDir, filename));
+                    Invoke((Action)delegate
+                    {
+                        Text = string.Format("Processing {0} on thread {1}",
+                            filename, Thread.CurrentThread.ManagedThreadId);
+                    });
+                });
+                Invoke((Action)delegate { Text = "Done!"; });
+            }
+            catch (OperationCanceledException ex) { Invoke((Action)delegate { Text = ex.Message; }); }
         }
 
-        private void btnProcessImages_Click(object sender, EventArgs e) => ProcessFiles();
+
+        private void btnProcessImages_Click(object sender, EventArgs e) => Task.Factory.StartNew(ProcessFiles);
+
+        private readonly CancellationTokenSource cancelToken = new();
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            cancelToken.Cancel();
+        }//789
     }
 }
